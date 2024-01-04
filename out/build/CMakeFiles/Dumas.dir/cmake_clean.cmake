1be56dfbf50b63564a6310fbf30f2775fc29d149
Marcos Flavio <mfghost69@gmail.com>
2024-01-04 19:00:51 -0300

    initial commit

---
+++ out/build/CMakeFiles/Dumas.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dumas.dir/src/main/main.cpp.o"
+  "CMakeFiles/Dumas.dir/src/main/main.cpp.o.d"
+  "Dumas"
+  "Dumas.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dumas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/CMakeFiles/Dumas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Dumas.
+# This may be replaced when dependencies are built.
