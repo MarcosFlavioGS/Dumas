19e97438b3047fdfba653b51e820123a667f0e94
Marcos Flavio <mfghost69@gmail.com>
2024-01-04 19:18:00 -0300

    Feature :using vec3 class

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,29 +1,28 @@
 #include "../../includes/Dumas.hpp"
+#include "../../includes/vec3.h"
+#include "../../includes/color.h"
 
 using namespace std;
 
-int main(void) {
-	// Image
-	int image_width = 256;
-	int image_height = 256;
+int main() {
 
-	// Render
-	cout << "P3\n" << image_width << ' ' << image_height << "\n255" << endl;
+    // Image
+    int image_width = 256;
+    int image_height = 256;
 
-	for (int j = 0; j < image_height; ++j) {
-		clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
-		for (int i = 0; i < image_width; ++i) {
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0;
+    // Render
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-			cout << ir << ' ' << ig << ' ' << ib << '\n';
-		}
-	}
-	clog << "\rDone.          \n";
-	return 0;
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+	return (0);
 }
